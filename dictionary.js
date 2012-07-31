25861581f18ea5ed3d96b25876e6160df08aa3ab
Timur R Mingaliev <timur@mingaliev.com>
2012-07-31 11:10:31 +0400

    Init commit

---
+++ dictionary.js
@@ -0,0 +1,9 @@
+var aeDictionary = new Dictionary([
+	{ ru: "реклама", en: "advertising"},
+	{ ru: "опыт", en: "experience"},
+	{ ru: "тормозит", en: "stops"},
+	{ ru: "сегодняшних", en: "todays"},
+	{ ru: "Создание", en: "Creating"},
+	{ ru: "первый", en: "first"},
+	{ ru: "Основная стадия проведения рыночного исследования вырождена.", en: "The main stage of market research is degenerate."}
+]);
---
+++ translate.js
@@ -0,0 +1,75 @@
+var Dictionary = function(dictionary){
+	var dict = dictionary;
+
+	dict.has = function(type, element){
+		for( var i = this.length; i--; ){
+			if( this[i][type] == element )
+				return i + 1;
+		};
+		return false;
+	};
+
+	dict.translate = function(type, text){
+		var i = this.has(type, text);
+		if( i ){
+			return this[i - 1];
+		}
+	}
+
+	return dict;
+};
+
+var Translate = function(dictionary){
+	var elements = document.body.getElementsByTagName('*');
+	var textNodes = [];
+	var dictionary = dictionary;
+
+	for( var i = elements.length; i--; ){
+		if( elements[i].nodeName in {	// Ignore Script and Link elements
+				"SCRIPT": "",
+				"LINK": ""
+			} )
+			continue;
+		var childs = elements[i].childNodes;
+		if( childs.length ){
+			for(var j = childs.length; j--; ){	
+				if( childs[j].nodeType == 3 ){
+					textNodes.push(childs[j]);
+				};
+			};
+		};
+	};
+
+	var clear = function(word){
+		return word.replace( /[,\.-:_"']/gi, '');
+	};
+
+	this.words = function(from, to){
+		for( var i = textNodes.length; i--; ){
+			var node = textNodes[i];
+			var text = node.textContent;
+			var words = text.split(' ');
+			for( var w = words.length; w--; ){
+				var word = words[w];
+				if( dictionary.has(from, clear(word)) )
+					words[w] = word.replace( clear(word), dictionary.translate(from, clear(word))[to] );
+			};
+			node.textContent = words.join(' ');
+		};
+		return this;
+	};
+
+	this.phrases = function(from, to){
+		for( var i = textNodes.length; i--; ){
+			var node = textNodes[i];
+			var text = node.textContent;
+			for( var d = dictionary.length; d--; ){
+				text = text.replace( dictionary[d][from], dictionary[d][to]);
+			};
+			node.textContent = text;
+		}
+		return this;
+	};
+
+	return this;
+};
