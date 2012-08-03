46b982c67cf5b65b56c2e0fef08e2f4046928583
Timur R Mingaliev <timur@mingaliev.com>
2012-08-03 17:03:24 +0400

    IE8 & IE7 fix

--- translate.js
+++ translate.js
@@ -47,14 +47,23 @@ var Translate = function(dictionary){
 	this.words = function(from, to){
 		for( var i = textNodes.length; i--; ){
 			var node = textNodes[i];
-			var text = node.textContent;
+			if( typeof node.textContent != 'undefined' ){
+				var text = node.textContent;
+			}else{
+				var text = node.nodeValue;
+			}
 			var words = text.split(' ');
 			for( var w = words.length; w--; ){
 				var word = words[w];
 				if( dictionary.has(from, clear(word)) )
 					words[w] = word.replace( clear(word), dictionary.translate(from, clear(word))[to] );
 			};
-			node.textContent = words.join(' ');
+
+			if( typeof node.textContent != 'undefined' ){
+				node.textContent = words.join(' ');
+			}else{
+				node.nodeValue = words.join(' ');
+			};
 		};
 		return this;
 	};
@@ -62,11 +71,21 @@ var Translate = function(dictionary){
 	this.phrases = function(from, to){
 		for( var i = textNodes.length; i--; ){
 			var node = textNodes[i];
-			var text = node.textContent;
+			if( typeof node.textContent != 'undefined' ){
+				var text = node.textContent;
+			}else{
+				var text = node.nodeValue;
+			};
+
 			for( var d = dictionary.length; d--; ){
 				text = text.replace( dictionary[d][from], dictionary[d][to]);
 			};
-			node.textContent = text;
+
+			if( typeof node.textContent != 'undefined' ){
+				node.textContent = text;
+			}else{
+				node.nodeValue = text;
+			};
 		}
 		return this;
 	};
@@ -75,11 +94,11 @@ var Translate = function(dictionary){
 		for( var i = elements.length; i--; ){
 			var element = elements[i];
 			if( element.nodeName == 'INPUT' ){
-				if( element.hasAttribute('value') ){
+				if( typeof element.value != 'undefined' ){
 					for( var d = dictionary.length; d--; ){
 						element.value = element.value.replace( dictionary[d][from], dictionary[d][to]);
 					};
-				}else if( element.hasAttribute('placeholder') && typeof element.placeholder != 'undefined' ){
+				}else if( typeof element.placeholder != 'undefined' ){
 					for( var d = dictionary.length; d--; ){
 						element.placeholder = element.placeholder.replace( dictionary[d][from], dictionary[d][to]);
 					};
